adaae87d4a58d03778a80aa2f064f8df28fc7057
dabakeketan <dabakeketan@gmail.com>
2025-11-10 18:17:33 +0530

    Webview implemented for products and coupons.

--- app/(home)/(fashion)/coupons.tsx
+++ app/(home)/(fashion)/coupons.tsx
@@ -1,20 +1,22 @@
-import { View, Text, TouchableOpacity, StyleSheet, ActivityIndicator, FlatList, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, ActivityIndicator, FlatList, ScrollView, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router';
 import { fetchData } from '@/services/baseservice';
 import { APIEndpoints } from '@/constants/appConstants';
 import { useTabDataContext } from './_layout';
+import { useNavigation } from '@react-navigation/native';
 
 const coupons = () => {
     const [loading, setLoading] = useState(false);
+    const [categoryData, setCategoryData] = useState<any>([]);
     const [coupons, setCoupons] = useState([]);
     let params: any = useLocalSearchParams();
     const [categoryId, setCategoryId] = useState(null);
     const { setSharedData }: any = useTabDataContext();
-
+    const navigationNative: any = useNavigation();
     useEffect(() => {
         if (params.categoryId && categoryId !== params.categoryId) {
-            console.log('categoryId', params.categoryId);
+            // console.log('categoryId', params.categoryId);
             setCategoryId(params.categoryId);
             getCategory(JSON.parse(params.categoryId));
         }
@@ -25,8 +27,10 @@ const coupons = () => {
             setLoading(true);
             const category = await fetchData(APIEndpoints.getCategory + '/' + categoryId);
             if (category && category.data && category.status.code === 200) {
+                setCategoryData(category.data[0] ? category.data[0] : {});
                 setCoupons(category?.data?.[0]?.vouchers?.length ? category?.data?.[0]?.vouchers : []);
-                setSharedData(category?.data?.[0]?.products?.length ? category?.data?.[0]?.products : []);
+                // setSharedData(category?.data?.[0]?.products?.length ? category?.data?.[0]?.products : []);
+                setSharedData(category.data[0] ? category.data[0] : {});
             } else {
             }
         } catch (err) {
@@ -36,25 +40,32 @@ const coupons = () => {
         }
     };
 
+    const openWebView = (affiliateLink: string, storeId: string) => {
+        if (affiliateLink) {
+            navigationNative.navigate('webview/[url]', { url: affiliateLink, storeId: storeId });
+        }
+    }
+
     return (
         <View style={{ flex: 1 }}>
             <View className="flex-row items-center py-3 px-4">
                 <View className="w-8 h-8 bg-gray-300 rounded-full mr-2" />
-                <Text className="text-xl font-semibold">Fashion</Text>
+                <Text className="text-xl font-semibold">{categoryData?.categoryName}</Text>
             </View>
             <ScrollView>
-            <View className="flex-row flex-wrap justify-between pt-2 px-4">
-                {coupons.map((item: any) => (
-                    <View key={item._id} className="w-[48%] bg-white rounded-2xl border border-gray-200 mb-4 p-3 shadow-sm">
-                        <View className="bg-gray-200 h-28 rounded-lg mb-3" />
-                        <Text className="text-blue-600 text-sm font-medium mb-1">{item.brand}</Text>
-                        <Text className="text-gray-700 text-sm mb-3 line-clamp-2 h-10">{item.voucherTitle}</Text>
-                        <TouchableOpacity className="bg-gray-800 py-2 rounded-lg">
-                            <Text className="text-white text-center font-semibold text-sm">Get Coupon Code</Text>
-                        </TouchableOpacity>
-                    </View>
-                ))}
-            </View>
+                <View className="flex-row flex-wrap justify-between pt-2 px-4">
+                    {coupons.map((item: any) => (
+                        <View key={item._id} className="w-[48%] bg-white rounded-2xl border border-gray-200 mb-4 p-3 shadow-sm">
+                            {/* <View className="bg-gray-200 h-28 rounded-lg mb-3" /> */}
+                            <Image className="bg-gray-200 h-28 rounded-lg mb-3" source={{ uri: item?.logoImage?.url }}></Image>
+                            <Text className="text-blue-600 text-sm font-medium mb-1">{item.brand}</Text>
+                            <Text className="text-gray-700 text-sm mb-3 line-clamp-2 h-10">{item.voucherTitle}</Text>
+                            <TouchableOpacity className="bg-gray-800 py-2 rounded-lg" onPress={() => openWebView(item.productLink, item.storeId)}>
+                                <Text className="text-white text-center font-semibold text-sm">Get Coupon Code</Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                </View>
             </ScrollView>
             {/* <FlatList
                 horizontal
--- app/(home)/(fashion)/index.tsx
+++ app/(home)/(fashion)/index.tsx
@@ -1,45 +1,52 @@
-import { View, Text, TouchableOpacity, ActivityIndicator, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator, StyleSheet, ScrollView, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router';
 import { useTabDataContext } from './_layout';
+import { useNavigation } from '@react-navigation/native';
 
 const Index = () => {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const { sharedData }: any = useTabDataContext();
+  const navigationNative: any = useNavigation();
   useEffect(() => {
-    console.log('sharedData', sharedData);
-    setProducts(sharedData);
-  },[sharedData]);
+    // console.log('sharedData', sharedData);
+    setProducts(sharedData && sharedData.products ? sharedData.products : []);
+  }, [sharedData]);
 
-  // const products: any = [];
+  const openWebView = (affiliateLink: string, storeId: string) => {
+    if (affiliateLink) {
+      navigationNative.navigate('webview/[url]', { url: affiliateLink, storeId: storeId });
+    }
+  }
   return (
     <View style={{ flex: 1 }}>
       <View className="flex-row items-center py-3 px-4">
         <View className="w-8 h-8 bg-gray-300 rounded-full mr-2" />
-        <Text className="text-xl font-semibold">Fashion</Text>
+        <Text className="text-xl font-semibold">{sharedData?.categoryName}</Text>
       </View>
       <ScrollView>
-      <View className="flex-row flex-wrap justify-between pt-2 px-4">
-        {products.map((item: any) => (
-          <View key={item._id} className="w-[48%] bg-white rounded-2xl border border-gray-200 mb-4 p-3 shadow-sm">
-            <View className="bg-gray-200 h-36 rounded-lg mb-3" />
-            <Text className="text-yellow-500 text-xs font-semibold mb-1">{item.productTag}</Text>
-            {/* <Text className="text-black font-semibold mb-1">{item.brand}</Text> */}
-            <Text className="text-gray-700 text-sm mb-3 line-clamp-2 h-10">{item.productTitle}</Text>
+        <View className="flex-row flex-wrap justify-between pt-2 px-4">
+          {products.map((item: any) => (
+            <View key={item._id} className="w-[48%] bg-white rounded-2xl border border-gray-200 mb-4 p-3 shadow-sm">
+              {/* <View className="bg-gray-200 h-36 rounded-lg mb-3" /> */}
+              <Image className="bg-gray-200 h-36 rounded-lg mb-3" source={{ uri: item?.images?.[0]?.url }}></Image>
+              <Text className="text-yellow-500 text-xs font-semibold mb-1">{item.productTag}</Text>
+              {/* <Text className="text-black font-semibold mb-1">{item.brand}</Text> */}
+              <Text className="text-gray-700 text-sm mb-3 line-clamp-2 h-10">{item.productTitle}</Text>
 
-            <View className="flex-row items-center justify-between">
-              <View className="flex-row items-center">
-                <Text className="text-black font-semibold text-base">₹{item.price}</Text>
-                <Text className="text-gray-400 text-sm line-through ml-2">₹{item.originalPrice}</Text>
+              <View className="flex-row items-center justify-between">
+                <View className="flex-row items-center">
+                  <Text className="text-black font-semibold text-base">₹{item.price}</Text>
+                  <Text className="text-gray-400 text-sm line-through ml-2">₹{item.originalPrice}</Text>
+                </View>
+                <TouchableOpacity className="bg-gray-800 px-3 py-1 rounded" onPress={() => openWebView(item.productLink, item.storeId)}>
+                  <Text className="text-white text-xs font-semibold">Buy Now</Text>
+                </TouchableOpacity>
               </View>
-              <TouchableOpacity className="bg-gray-800 px-3 py-1 rounded">
-                <Text className="text-white text-xs font-semibold">Buy Now</Text>
-              </TouchableOpacity>
             </View>
-          </View>
-        ))}
-      </View>
+          ))}
+        </View>
       </ScrollView>
       {
         loading && (
